3c2e77bba0aa96c55a00ed33323df1991025aa59
Stephenamankwa <160183785+Stephenamankwa@users.noreply.github.com>
2024-05-27 11:41:18 +0000

    Update App.js

--- rn-assignment2-ID-11039578/App.js
+++ rn-assignment2-ID-11039578/App.js
@@ -1,20 +1,30 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text styles={styles.text}>
+        My name is <Text style={styles.boldText}>Stephen</Text>
+        </Text>
       <StatusBar style="auto" />
     </View>
-  );
+  );s
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ADD8E6',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
   },
 });
